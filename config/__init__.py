b4d7e34bbf3c8462493cbad4e958d2466b3519e4
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-05-12 10:23:34 +0800

    update

--- config/__init__.py
+++ config/__init__.py
@@ -5,8 +5,12 @@ from .model_config import m_config
 def build_config(dataset, model_name):
     # build dataset config
     dataset_config = d_config[dataset]
+    print(dataset_config)
+
     # build model config
     model_config = m_config[model_name]
-    print(m_config)
+    print(model_config)
+
+    
     return dataset_config, model_config
     
