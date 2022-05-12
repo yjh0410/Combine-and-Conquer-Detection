c114f8cdeb9259073f62c97368fb0bf3d0f7e542
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-05-12 12:33:45 +0800

    update

--- config/model_config.py
+++ config/model_config.py
@@ -364,7 +364,7 @@ m_config = {
         # training configuration
         'max_epoch': 200,
         'no_aug_epoch': 15,
-        'batch_size': 32,
+        'batch_size': 64,
         'base_lr': 0.01 / 64.,
         'min_lr_ratio': 0.01,
         # optimizer
@@ -476,7 +476,7 @@ m_config = {
         # training configuration
         'max_epoch': 200,
         'no_aug_epoch': 15,
-        'batch_size': 16,
+        'batch_size': 32,
         'base_lr': 0.01 / 64.,
         'min_lr_ratio': 0.01,
         # optimizer
@@ -644,7 +644,7 @@ m_config = {
         # training configuration
         'max_epoch': 200,
         'no_aug_epoch': 15,
-        'batch_size': 32,
+        'batch_size': 64,
         'base_lr': 0.01 / 64.,
         'min_lr_ratio': 0.01,
         # optimizer
