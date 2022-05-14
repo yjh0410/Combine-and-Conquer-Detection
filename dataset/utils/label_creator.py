73e9fc68cbd810583fe948a2bf6dfbcf639af8c7
yjh0410 <1394571815@qq.com>
2022-05-14 20:57:29 +0800

    train CCDet-R18 on coco

--- dataset/utils/label_creator.py
+++ dataset/utils/label_creator.py
@@ -64,15 +64,15 @@ class HMPCreator(object):
 
             gt_heatmaps[y1s:y2s, x1s:x2s, label] = np.maximum(cur_hmp, prev_hmp)
 
-            # multi positive samples
-            for i in range(grid_x - 1, grid_x + 2):
-                for j in range(grid_y - 1, grid_y + 2):
-                    if (j >=0 and j < gt_bboxes.shape[0]) and \
-                        (i >=0 and i < gt_bboxes.shape[1]):
-                        gt_bboxes[j, i] = np.array([x1, y1, x2, y2])
-                        img_area = (img_h * img_w)
-                        box_area = (x2 - x1) * (y2 - y1)
-                        gt_bboxes_weights[j, i] = 2.0 - box_area / img_area
+            # # multi positive samples
+            # for i in range(grid_x - 1, grid_x + 2):
+            #     for j in range(grid_y - 1, grid_y + 2):
+            #         if (j >=0 and j < gt_bboxes.shape[0]) and \
+            #             (i >=0 and i < gt_bboxes.shape[1]):
+            #             gt_bboxes[j, i] = np.array([x1, y1, x2, y2])
+            #             img_area = (img_h * img_w)
+            #             box_area = (x2 - x1) * (y2 - y1)
+            #             gt_bboxes_weights[j, i] = 2.0 - box_area / img_area
         
         targets = np.concatenate([
             gt_heatmaps, 
