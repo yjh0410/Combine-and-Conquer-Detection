7f5b0ade4a08e45902b7c86043575bb4c3d6e705
yjh0410 <1394571815@qq.com>
2022-05-14 08:43:33 +0800

    train CCDet-S on voc

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python train.py \
             --cuda \
             -d voc \
-            -v ccdet_r101 \
+            -v ccdet_2 \
             --ema \
             --fp16 \
             --eval_epoch 10
