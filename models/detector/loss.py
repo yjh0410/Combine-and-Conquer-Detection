eb120beae7c720ae2bbe609753cef887409b6953
yjh0410 <1394571815@qq.com>
2022-05-16 00:06:13 +0800

    update

--- models/detector/loss.py
+++ models/detector/loss.py
@@ -32,9 +32,9 @@ class Criterion(object):
         neg_inds = target.lt(1).float()
 
         neg_weights = torch.pow(1.0 - target, 4)
-        pred = torch.max(pred, torch.ones_like(pred) * 1e-12)
+        # pred = torch.max(pred, torch.ones_like(pred) * 1e-12)
 
-        print(torch.log(pred).sum(), torch.log(1 - pred).sum(),)
+        print(torch.log(pred).sum(), torch.log(1.0 - pred).sum(),)
         pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
         neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
 
