d7b7872248efdbc20633ba7e98584355a088d9ee
yjh0410 <1394571815@qq.com>
2022-05-24 09:10:48 +0800

    update

--- config/model_config.py
+++ config/model_config.py
@@ -38,7 +38,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -91,7 +91,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -149,7 +149,7 @@ m_config = {
         # training configuration
         'max_epoch': 200,
         'no_aug_epoch': 15,
-        'batch_size': 16,
+        'batch_size': 32,
         'base_lr': 0.01 / 64.,
         'min_lr_ratio': 0.01,
         # optimizer
@@ -197,7 +197,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -250,7 +250,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -303,7 +303,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -356,7 +356,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -410,7 +410,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -463,7 +463,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
@@ -516,7 +516,7 @@ m_config = {
         'test_topk': 100,
         # loss
         'loss_hmp_weight': 1.0,
-        'loss_reg_weight': 1.0,
+        'loss_reg_weight': 5.0,
         'loss_iou_weight': 1.0,
         # training configuration
         'max_epoch': 200,
