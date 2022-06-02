ad08fa80092a280b868eb01443e27a7db667268a
yjh0410 <1394571815@qq.com>
2022-06-02 08:59:58 +0800

    train ccdet_d19 on coco

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python train.py \
             --cuda \
             -d coco \
-            -v ccdet_r101 \
+            -v ccdet_d19 \
             --ema \
             --fp16 \
             --eval_epoch 10
