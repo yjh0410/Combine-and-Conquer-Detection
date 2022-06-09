80506ae65518cc3b7e6b82b573fd517c34451a0b
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-09 10:04:06 +0800

    update

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -1,4 +1,4 @@
-from .voc import VOCDetection, VOCAnnotationTransform, VOC_CLASSES
+from .voc import VOCDetection, VOC_CLASSES
 from .coco import COCODataset, coco_class_labels, coco_class_index
-from .widerface import WIDERFaceDetection, WIDERFaceAnnotationTransform, WIDERFace_CLASSES
+from .widerface import WIDERFaceDetection, WIDERFace_CLASSES
 from .crowdhuman import CrowdHumanDetection, CrowdHuman_CLASSES
