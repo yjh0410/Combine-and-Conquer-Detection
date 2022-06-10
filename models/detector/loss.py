bea55b771d10473e4b2c6a7b5b0836375616162d
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-10 12:07:03 +0800

    train ccdet_r18 on widerface

--- models/detector/loss.py
+++ models/detector/loss.py
@@ -30,20 +30,14 @@ class Criterion(object):
 
         pos_inds = target.eq(1).float()
         neg_inds = target.lt(1).float()
+        num_pos  = pos_inds.float().sum().clamp(1.0)
 
         neg_weights = torch.pow(1.0 - target, 4)
 
         pos_loss = torch.log(pred) * torch.pow(1 - pred, 2) * pos_inds
         neg_loss = torch.log(1 - pred) * torch.pow(pred, 2) * neg_weights * neg_inds
 
-        num_pos  = pos_inds.float().sum()
-        pos_loss = pos_loss.sum()
-        neg_loss = neg_loss.sum()
-
-        if num_pos == 0:
-            loss = -neg_loss
-        else:
-            loss = -(pos_loss + neg_loss) / num_pos
+        loss = -(pos_loss + neg_loss).sum() / num_pos
 
         return loss
 
