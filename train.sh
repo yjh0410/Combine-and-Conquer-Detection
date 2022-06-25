fe26bb5e12ee866347758839ad931c7439cf6b28
HIT-k544 <19B908049@stu.hit.edu.cn>
2022-06-25 11:38:08 +0800

    test ccdet_r18 on widerface

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 python train.py \
             --cuda \
             -d widerface \
-            -v ccdet_r18 \
+            -v ccdet_r50 \
             --ema \
             --fp16 \
             --eval_epoch 10
